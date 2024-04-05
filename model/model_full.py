2dc158b70eef9f3f5ca2359621daf03f75f78fa5
msc2024 <msc2024@ubuntu.com>
2024-04-05 14:25:12 +0800

    update names

--- model/model_full.py
+++ model/model_full.py
@@ -1,128 +1,97 @@
 import functools
-
 import torch
 import torch.nn as nn
 from .networks import Encoder, Denoiser, Decoder, SEBlock, Chuncked_Self_Attn_FM, AutoencoderBackbone, get_norm_layer
 from base.base_model import BaseModel
 from utils.util import torch_laplacian
 
-def normalize_to_0_1(tensor):
-    min_val = torch.min(tensor)
-    max_val = torch.max(tensor)
-    normalized_tensor = (tensor - min_val) / (max_val - min_val)
-    return normalized_tensor
-# class DeblurModel(nn.Module):
-#     """
-#     去模糊模型,包括编码器、去噪器和解码器  
-#     输入:
-#         - blurred_image (N, 1, 256, 320)
-#         - events (N, 13, 256, 320)
-#     输出: 
-#         - log_diff (N, 1, 256, 320)  
-#         - sharp_image (N, 1, 256, 320)
-#     """
 class DefaultModel(BaseModel):
-    """
-    去模糊模型,包括编码器、去噪器和解码器,使用skip connection
-    输入:
-        - blurred_image (N, 1, 256, 320)
-        - events (N, 13, 256, 320)
-    输出:
-        - log_diff (N, 1, 256, 320)
-        - sharp_image (N, 1, 256, 320)
-    """
     def __init__(self, init_dim=64, n_ev=13, grid=(8, 10), norm_type='instance', use_dropout=False, rgb=False):
         super(DefaultModel, self).__init__()
 
         norm_layer = get_norm_layer(norm_type)
-        if type(norm_layer) == functools.partial:  # no need to use bias as BatchNorm2d has affine parameters
+        if type(norm_layer) == functools.partial:
             use_bias = norm_layer.func != nn.BatchNorm2d
         else:
             use_bias = norm_layer != nn.BatchNorm2d
 
         self.rgb = rgb
-        self.encoder_b = nn.ModuleList([
-            Encoder(1, 32),   # (N, 1, 256, 320) -> down(N, 32, 128, 160), self(N, 32, 256, 320)
-            Encoder(32, 64),  # (N, 32, 128, 160) -> (N, 64, 64, 80), (N, 64, 128, 160)
-            Encoder(64, 128), # (N, 64, 64, 80) -> (N, 128, 32, 40), (N, 128, 64, 80)
-            Encoder(128, 256), # (N, 128, 32, 40) -> (N, 256, 16, 20), (N, 256, 32, 40)
-
+        self.encoder_blurred = nn.ModuleList([
+            Encoder(1, 32),
+            Encoder(32, 64),
+            Encoder(64, 128),
+            Encoder(128, 256),
         ])
-        self.encoder_e = nn.ModuleList([
-            Encoder(13, 32),  # (N, 13, 256, 320) -> (N, 32, 128, 160), (N, 32, 256, 320)
-            Encoder(32, 64),  # (N, 32, 128, 160) -> (N, 64, 64, 80), (N, 64, 128, 160)  
-            Encoder(64, 128), # (N, 64, 64, 80) -> (N, 128, 32, 40), (N, 128, 64, 80)
-            Encoder(128, 256), # (N, 128, 32, 40) -> (N, 256, 16, 20), (N, 256, 32, 40)
+        self.encoder_events = nn.ModuleList([
+            Encoder(13, 32),
+            Encoder(32, 64),
+            Encoder(64, 128),
+            Encoder(128, 256),
         ])
-        self.seb1 = SEBlock(512, 16)
-        self.seb2 = nn.Sequential(
-            nn.Conv2d(2, 16, kernel_size=3, stride=1, padding=1, bias=use_bias), # 增加融合层的感受野和非线性
+        self.se_block1 = SEBlock(512, 16)
+        self.se_block2 = nn.Sequential(
+            nn.Conv2d(2, 16, kernel_size=3, stride=1, padding=1, bias=use_bias),
             nn.ReLU(inplace=True),
             SEBlock(16, 2),
             nn.Conv2d(16, 1, kernel_size=1, stride=1, bias=use_bias)
         )
-        self.denoiser = Denoiser(512, 256) # (N, 512, 16, 20) -> (N, 256, 16, 20)
+        self.denoiser = Denoiser(512, 256)
         self.self_attn = Chuncked_Self_Attn_FM(256, latent_dim=16, subsample=True, grid=grid)
         self.decoder = nn.ModuleList([
-            Decoder(256, 128), # (N, 256, 16, 20) -> (N, 128, 32, 40)
-            Decoder(128, 64),  # (N, 128, 32, 40) -> (N, 64, 64, 80)
-            Decoder(64, 32),   # (N, 64, 64, 80) -> (N, 32, 128, 160)
-            Decoder(32, 1),   # (N, 64, 64, 80) -> (N, 32, 128, 160)
+            Decoder(256, 128),
+            Decoder(128, 64),
+            Decoder(64, 32),
+            Decoder(32, 1),
         ])
-        self.up = nn.Sequential(
-                nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True) # (N, 1, 128, 160) -> (N, 1, 256, 320)
-            )
-        self.Bi_denoise = nn.Sequential(
-                AutoencoderBackbone(1, output_nc=init_dim, n_downsampling=2, n_blocks=4, norm_type=norm_type,
-                                    use_dropout=use_dropout),
-                nn.Conv2d(init_dim, 1, kernel_size=1, stride=1, bias=use_bias),
-                nn.Tanh()
-            )
+        self.upsample = nn.Sequential(
+            nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        )
+        self.bi_denoiser = nn.Sequential(
+            AutoencoderBackbone(1, output_nc=init_dim, n_downsampling=2, n_blocks=4, norm_type=norm_type, use_dropout=use_dropout),
+            nn.Conv2d(init_dim, 1, kernel_size=1, stride=1, bias=use_bias),
+            nn.Tanh()
+        )
         self.tanh = nn.Tanh()
 
     def forward(self, blurred_image, noised_b_image, events):
-        b_code = torch_laplacian(blurred_image)
-        e_code = events
-        b_codes = []
-        e_codes = []
+        blurred_code = torch_laplacian(blurred_image)
+        events_code = events
+        blurred_codes = []
+        events_codes = []
 
         bi_gamma = noised_b_image ** (1 / 2.2)
-        bi_clean = torch.clamp(self.Bi_denoise(bi_gamma) + bi_gamma, min=0, max=1) ** 2.2
+        bi_clean = torch.clamp(self.bi_denoiser(bi_gamma) + bi_gamma, min=0, max=1) ** 2.2
         print(bi_clean.shape)
 
-        for b_enc, e_enc in zip(self.encoder_b, self.encoder_e):
-            b_code, b_skip = b_enc(b_code)
-            e_code, e_skip = e_enc(e_code)
-            b_codes.append(b_skip)
-            e_codes.append(e_skip)
+        for blurred_enc, events_enc in zip(self.encoder_blurred, self.encoder_events):
+            blurred_code, blurred_skip = blurred_enc(blurred_code)
+            events_code, events_skip = events_enc(events_code)
+            blurred_codes.append(blurred_skip)
+            events_codes.append(events_skip)
 
-        code = torch.cat([b_code, e_code], 1)
-        print("code.shape after cat", code.shape) # ([2, 512, 16, 20])
-        code = self.seb1(code)
+        code = torch.cat([blurred_code, events_code], 1)
+        print("code.shape after cat", code.shape)
+        code = self.se_block1(code)
         code = self.denoiser(code)
-        print("code.shape", code.shape) # [1, 256, 16, 20])
+        print("code.shape", code.shape)
         code = self.self_attn(code)
-        for dec, b_skip, e_skip in zip(self.decoder, reversed(b_codes), reversed(e_codes)):
+
+        for dec, blurred_skip, events_skip in zip(self.decoder, reversed(blurred_codes), reversed(events_codes)):
             print("++++++++++++++++++++++++++++++++++++++++++++++++++")
-            skips = torch.cat([b_skip, e_skip], 1)
+            skips = torch.cat([blurred_skip, events_skip], 1)
             print("skips", skips.shape)
             code = dec(code, skips)
             print("code.shape", code.shape)
-        print("code.shape after decode",code.shape)
-        # code = self.seb2(code)
 
-        # flow = self.flow_block(code)
-        # code = self.up(code)
-        # log_diff = code
+        print("code.shape after decode", code.shape)
+
         cated = torch.cat((bi_clean, code), dim=1)
-        fused = self.seb2(cated)
+        fused = self.se_block2(cated)
         print(fused.shape)
         log_diff = torch.neg(fused)
         print(log_diff.shape)
         log_diff = self.tanh(log_diff)
         sharp_image = log_diff + bi_clean
-        # sharp_image = torch.clamp(sharp_image,min=0,max=1)
-        # sharp_image = normalize_to_0_1(sharp_image)
         print(sharp_image.shape)
-        # return flow, log_diff, sharp_image, log_diff
+
         return bi_clean, log_diff, sharp_image, log_diff
